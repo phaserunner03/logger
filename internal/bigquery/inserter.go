b5061c5078bc6436491da75598fd67700270f226
ykodwani01 <yashkodwani2003@gmail.com>
2025-06-09 15:13:00 +0530

    Added PubSub

--- internal/bigquery/inserter.go
+++ internal/bigquery/inserter.go
@@ -4,31 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 	"github.com/phaserunner03/logging/configs"
 	"cloud.google.com/go/bigquery"
 	"google.golang.org/api/option"
 )
 
-type BQLogRow struct {
-	Timestamp      time.Time `bigquery:"timestamp"`       // REQUIRED
-	Severity       string    `bigquery:"severity"`        // NULLABLE
-	LogName        string    `bigquery:"log_name"`        // NULLABLE
-	TextPayload    string    `bigquery:"text_payload"`    // NULLABLE
-	JsonPayload    string    `bigquery:"json_payload"`    // NULLABLE (JSON type in BigQuery)
-	InsertID       string    `bigquery:"insert_id"`       // NULLABLE
-	ResourceType   string    `bigquery:"resource_type"`   // NULLABLE
-	ResourceLabels string    `bigquery:"resource_labels"` // NULLABLE (JSON type)
-	HTTPRequest    string    `bigquery:"http_request"`    // NULLABLE (JSON type)
-	Trace          string    `bigquery:"trace"`           // NULLABLE
-	SpanID         string    `bigquery:"span_id"`         // NULLABLE
-	SourceLocation string    `bigquery:"source_location"` // NULLABLE (JSON type)
-	Labels         string    `bigquery:"labels"`          // NULLABLE (JSON type)
-	ServiceName    string    `bigquery:"service_name"`    // NULLABLE // Added service name field
-}
 
 // InsertLogs inserts log entries into BigQuery
-func InsertLogs(ctx context.Context, rows []BQLogRow) error {
+func InsertLogs(ctx context.Context, rows []configs.BQLogRow) error {
 	if len(rows) == 0 {
 		return nil
 	}
