91726ce26e4edd8a25d596f524419c13916e0f22
bhavya418 <shahb3703@gmail.com>
2025-06-17 13:34:34 +0530

    pubsub module added

--- internal/analysis/handle_error.go
+++ internal/analysis/handle_error.go
@@ -1,12 +1,11 @@
 package analysis
 
+
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"google.golang.org/api/option"
-	"cloud.google.com/go/pubsub"
 	"github.com/phaserunner03/logging/configs"
+	"github.com/phaserunner03/logging/internal/prodsub"
 )
 
 func HandleError(ctx context.Context, bqRows []configs.BQLogRow) error {
@@ -29,44 +28,18 @@ func HandleError(ctx context.Context, bqRows []configs.BQLogRow) error {
 
 	
 
-	client, err := pubsub.NewClient(ctx, GCP_ProjectID, option.WithCredentialsFile(credentialsPath))
+	publisher,err := prodsub.NewPublisher(ctx,GCP_ProjectID,topicID,credentialsPath)
 	if err != nil {
 		return fmt.Errorf("failed to create Pub/Sub client: %v", err)
 	}
-	defer client.Close()
+	
+	defer publisher.Close()
 
-	topic := client.Topic(topicID)
-	if topic == nil {
-		return fmt.Errorf("failed to retrieve Pub/Sub topic: %s", topicID)
-	}
-	defer topic.Stop()
-
+	
 	for _, row := range bqRows {
-		switch row.Severity {
-		case "ERROR", "PANIC", "WARNING":
-			// allowed
-		default:
-			continue
+		if err:= publisher.PublishLog(ctx,row); err!= nil{
+			return fmt.Errorf("failed to publish message: %v",err)
 		}
-
-		data, err := json.Marshal(row)
-		if err != nil {
-			return fmt.Errorf("failed to marshal row to JSON: %v", err)
-		}
-		//send only error
-		msg := &pubsub.Message{
-			Data: data,
-			Attributes: map[string]string{
-				"service_name": row.ServiceName,
-			},
-		}
-		result := topic.Publish(ctx, msg)
-		// Block until the result is returned and log any errors
-		id, err := result.Get(ctx)
-		if err != nil {
-			return fmt.Errorf("failed to publish message to Pub/Sub: %v", err)
-		}
-		fmt.Printf("Message published with ID: %s\n", id)
 	}
 
 	return nil
